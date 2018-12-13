7fa7da933441de06268701e6599e15f33073fcc6
Suresh Kondati Natarajan <suresh0807@gmail.com>
2018-12-13 12:05:44 +0000

    Create error_bar.sh

---
+++ error_bar.sh
@@ -0,0 +1,7 @@
+#average minus plus
+a=`awk -v s=$1 'BEGIN{min=10000000000}{if($s<min) {min=$s}} END{print  min}' $2`
+b=`awk -v s=$1 'BEGIN{max=-10000000000}{if($s>max) {max=$s}} END{print max}' $2`
+c=`awk -v s=$1 '{sum +=$s; n++} END {if(n>0) print sum /n;}' $2`
+d=`echo "$c-$a"|bc -l`
+e=`echo "$b-$c"|bc -l`
+echo "$c $e $d"
