3aaccc68a6ed710c14b7158509aa11da101b4cd6
Suresh Kondati Natarajan <suresh0807@gmail.com>
2019-03-01 18:58:54 +0000

    Update error_bar.sh

--- error_bar.sh
+++ error_bar.sh
@@ -1,4 +1,4 @@
-#average minus plus
+#average minus plus - must be all positive values!
 a=`awk -v s=$1 'BEGIN{min=10000000000}{if($s<min) {min=$s}} END{print  min}' $2`
 b=`awk -v s=$1 'BEGIN{max=-10000000000}{if($s>max) {max=$s}} END{print max}' $2`
 c=`awk -v s=$1 '{sum +=$s; n++} END {if(n>0) print sum /n;}' $2`
