c60cab7c9d77e1182cc655034f281b2773c3fddd
Suresh Kondati Natarajan <suresh0807@gmail.com>
2019-03-02 17:01:58 +0000

    Delete POSCAR2xyz.py

--- POSCAR2xyz.py
+++
@@ -1,5 +0,0 @@
-#with ase installed, use this script as 'python POSCAR2xyz.py' to convert POSCAR in the folder to POSCAR.xyz
-from ase import Atoms
-from ase.io import read,write
-a = read('POSCAR')
-write('POSCAR.xyz', a)
